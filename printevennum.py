4654920ff8269a740d9c0f8cf644495873cdba07
Jaime Rogstad <66330786+jrog20@users.noreply.github.com>
2021-04-15 18:35:09 -0400

    Add files via upload

---
+++ printevennum.py
@@ -0,0 +1,17 @@
+"""
+File: printevennum.py
+------------------
+This program will print three even numbers, starting with 0.
+"""
+
+def main():
+    """
+    print 0, 2, 4.
+    """
+
+    for i in range(3):
+        print(i * 2)
+
+
+if __name__ == '__main__':
+    main()
